750486920f7eaed104d8951a7d15cf0941bea749
kierepkae <kierepkaedyta@gmail.com>
2019-08-05 16:21:19 +0100

    add new features - change border color and name color with parameters

--- app.rb
+++ app.rb
@@ -16,8 +16,16 @@ get '/hello_world' do
 end
 
 
-get '/cat' do
+get '/random-cat' do
   @cat_name = ['Miouw', 'Jerry', 'Tom'].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name_color = params[:namecolor]|| "#49B0D0"
+  @color = params[:color] || "#86CB1E"
+  @cat_name = params[:name]
   erb(:index)
 end
 
