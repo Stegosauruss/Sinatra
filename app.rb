f60fbcfdde30731d97b7622234c6eabb4f05f287
kierepkae <kierepkaedyta@gmail.com>
2019-08-05 15:35:25 +0100

    add /cat route

--- app.rb
+++ app.rb
@@ -15,4 +15,13 @@ get '/hello_world' do
   "Hello, world!"
 end
 
+
+get '/cat' do
+
+  "<div style = 'border: 3px dashed red; max-width: 400px' >
+  <img style = 'width: 400px'src='http://bit.ly/1eze8aE'>
+ </div>"
+
+end
+
 set :session_secret, 'super secret'
